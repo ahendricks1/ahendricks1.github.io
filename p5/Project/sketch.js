4d00c57fc789a426af46aaf1fe43463dd3e9f4ea
Austin Hendricks <ahendricks2@cocc.edu>
2017-10-20 15:38:19 -0700

    Added personal project pathway to p5 index.

--- p5/Project/sketch.js
+++ p5/Project/sketch.js
@@ -1,12 +1,17 @@
 function setup() {
-  createCanvas(480, 320);
+  createCanvas(500,400);
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
+background(223,215,160);
+fill(204,0,110,200);
+ellipse(132,82,200,200);
+fill(0,220,118,200);
+ellipse(228,-16,200,200);
+fill(40,0,237,200);
+ellipse(268,118,200,200);
+
+quad(158, 55, 199, 14, 392, 66, 351, 107);
+triangle(347, 54, 392, 9, 392, 66);
+triangle(158, 55, 290, 91, 290, 112);
 }
